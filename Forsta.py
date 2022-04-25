60417412ceec097dcf4c0bc8ce845e29f2cef4e1
Felix <Felix.vigren@bredband2.com>
2022-04-25 13:34:41 +0200

    funkar

--- Forsta.py
+++ Forsta.py
@@ -7,19 +7,19 @@ file = open('skriv.txt','r', encoding='utf8')
 new_dict=dict()
 read_data = file.read()
 split_data = read_data.split()
-stripped_data = split_data.strip()
-for line in stripped_data:
-    if line in new_dict.keys():
-        new_dict[line] = int(new_dict.get(line))+1
-    else: new_dict[line] = 1
+for line in split_data:
+    stripped_line = line.strip()
+    if stripped_line in new_dict.keys():
+        new_dict[stripped_line] = int(new_dict.get(stripped_line))+1
+    else: new_dict[stripped_line] = 1
 print(new_dict)
 
 
 
 
-quick(a, 0, len(a)-1)  
-print("\nAfter sorting array elements are - ")  
-printArr(a)  
+#quick(a, 0, len(a)-1)  
+#print("\nAfter sorting array elements are - ")  
+#printArr(a)  
 
 #
 #word_count = read_data.lower().count(search_word_count)
