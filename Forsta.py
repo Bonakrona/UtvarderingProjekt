6ea6e9d23d8195d023f98ea9925f63074323ed9a
Felix <Felix.vigren@bredband2.com>
2022-04-25 13:19:15 +0200

    la till text fil

--- Forsta.py
+++ Forsta.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from logging.handlers import BaseRotatingHandler
 
 
@@ -9,11 +8,11 @@ file = open('skriv.txt','r', encoding='utf8')
 new_dict=dict()
 read_data = file.read()
 split_data = read_data.split()
-for line in split_data:
-    stripped_line = line.strip()
-    if stripped_line in new_dict.keys():
-        new_dict[stripped_line] = int(new_dict.get(stripped_line))+1
-    else: new_dict[stripped_line] = 1
+stripped_data = split_data.strip()
+for line in stripped_data:
+    if line in new_dict.keys():
+        new_dict[line] = int(new_dict.get(line))+1
+    else: new_dict[line] = 1
 print(new_dict)
 
 
@@ -49,23 +48,11 @@ def printArr(a): # function to print the array
         print (a[i], end = " ")  
   
       
-a = [68, 13, 1, 49, 58]  
-print("Before sorting array elements are - ")  
-printArr(a)  
+
 quick(a, 0, len(a)-1)  
 print("\nAfter sorting array elements are - ")  
 printArr(a)  
+
 #
 #word_count = read_data.lower().count(search_word_count)
 #print(f"The word '{search_word_count}' appeard {word_count} times.")
-=======
-print("hello man hej på  diiiiig man")
-if dinMamma is Snygg:
-    Banga
-
-
-for freddi in felix:
-    if kyss:
-        giftermå
-        return
->>>>>>> 794c0a5e68076efa52a9c782f111a91a4cde379d
