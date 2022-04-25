09df113e2edd8d8b8a87421bd8d6a7a6166f24c3
Felix <Felix.vigren@bredband2.com>
2022-04-25 12:23:29 +0200

    första verision

--- Forsta.py
+++ Forsta.py
@@ -1,6 +1,20 @@
 from logging.handlers import BaseRotatingHandler
 
 
-print("hello man hej på  diiiiig man")
-if dinMamma is Snygg:
-    Banga
+
+
+#search_word_count = input('Enter the word')
+file = open('skriv.txt','r', encoding='utf8')
+new_dict=dict()
+read_data = file.read()
+split_data = read_data.split()
+for line in split_data:
+    stripped_line = line.strip()
+    if stripped_line in new_dict.keys():
+        new_dict[stripped_line] = int(new_dict.get(stripped_line))+1
+    else: new_dict[stripped_line] = 1
+print(new_dict)
+
+#
+#word_count = read_data.lower().count(search_word_count)
+#print(f"The word '{search_word_count}' appeard {word_count} times.")
