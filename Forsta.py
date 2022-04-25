97ee69f85d844ddf82489c69b208754e565ccec3
Felix <Felix.vigren@bredband2.com>
2022-04-25 13:23:29 +0200

    delade upp filerna i main delen och quicksort

--- Forsta.py
+++ Forsta.py
@@ -1,7 +1,6 @@
 from logging.handlers import BaseRotatingHandler
 
-
-
+from quicksort import printArr, quick, partition
 
 #search_word_count = input('Enter the word')
 file = open('skriv.txt','r', encoding='utf8')
@@ -16,38 +15,7 @@ for line in stripped_data:
 print(new_dict)
 
 
-#function that consider last element as pivot,   
-#place the pivot at its exact position, and place   
-#smaller elements to left of pivot and greater   
-#elements to right of pivot.   
-  
-def partition (a, start, end):  
-    i = (start - 1)  
-    pivot = a[end] # pivot element  
-      
-    for j in range(start, end):  
-        # If current element is smaller than or equal to the pivot  
-        if (a[j] <= pivot):  
-            i = i + 1  
-            a[i], a[j] = a[j], a[i]  
-      
-    a[i+1], a[end] = a[end], a[i+1]  
-  
-    return (i + 1)  
-      
-# function to implement quick sort   
-def quick(a, start, end): # a[] = array to be sorted, start = Starting index, end = Ending index      
-    if (start < end):  
-        p = partition(a, start, end) # p is partitioning index  
-        quick(a, start, p - 1)  
-        quick(a, p + 1, end)  
-  
-          
-def printArr(a): # function to print the array  
-    for i in range(len(a)):  
-        print (a[i], end = " ")  
-  
-      
+
 
 quick(a, 0, len(a)-1)  
 print("\nAfter sorting array elements are - ")  
